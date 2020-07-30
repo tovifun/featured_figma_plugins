deae77d52e020743bf97016a6fe28129d8794738
tovi <twowei@live.com>
2020-07-30 13:20:09 +0800

    issue6

--- src/pages/issue5.js
+++ src/pages/issue5.js
@@ -1,37 +1,35 @@
 import React from 'react'
 import Layout from '../layouts'
-import styled from 'styled-components'
 import Cell from '../components/Cell'
+import issue5 from '../data/issue5.json'
 import Archive from '../components/Archive'
-import About from '../components/About'
-import Subscribe from '../components/Subscribe'
 import issues from '../data/issues.json'
-import issue6 from '../data/issue6.json'
+import styled from 'styled-components'
+import Subscribe from '../components/Subscribe'
 import { COLORS } from '../values/colors'
-// import ComponentA from '../components/ComponentA'
 
-const SectionCaption = styled.div`
+const SectionBread = styled.div`
   margin: 0 auto;
   font-weight: 600;
   font-size: 16px;
-  color: ${COLORS.neutral_4};
+  color: #130C31;
   max-width: 636px;
-  padding: 58px 50px 10px;
+  padding: 160px 50px 10px;
   display: flex;
   -webkit-box-align: center;
   align-items: center;
   vertical-align: baseline;
   @media (max-width:640px){
-    padding: 58px 24px 10px;
+    padding: 100px 24px 10px;
   }
 `
 
-const CaptionTitle = styled.p`
+const BreadHome = styled.p`
   margin:0;
 `
 
-const CaptionDate = styled.p`
-  margin:0;
+const BreadCurrent = styled.p`
+margin:0;
   margin-left: 12px;
   color: ${COLORS.neutral_2};
   font-weight:500;
@@ -46,7 +44,9 @@ const SectionCellGroup = styled.div`
   grid-gap: 32px 56px;
   justify-items: center;
   
-
+  a{
+    width:100%;
+  }
   @media (max-width:1280px){
     grid-template-columns: repeat(3, 1fr);
   }
@@ -66,10 +66,6 @@ const SectionCellGroup = styled.div`
       padding: 12px 24px 8px 24px;
       grid-gap: 28px 32px;
   }
-
-  a{
-    width:100%;
-  }
 `
 
 const SectionArchive = styled.div`
@@ -78,33 +74,20 @@ const SectionArchive = styled.div`
       padding: 70px 24px 70px;
     }
 `
-const SectionAbout = styled.div`
-  padding: 64px 50px 100px 50px;
-  background:#F7F3FA;
-  @media (max-width:640px){
-      padding: 64px 24px 64px;
-    }
-`
 
 const SectionSubscribe = styled.div`
 `
 
-const IndexPage = () => (
+const SubscribePage = () => (
   <Layout>
-    <div className="Hero">
-      <div className="HeroGroup">
-        <h1><span className="GradientText">Featured</span><br />Figma<br />Plugins.</h1>
-        <p>每月的最后一天，给你推荐当月更新的、值得尝试的 Figma 插件。<br />下一次推送时间：7月31日。可以关注公众号获得更新提醒。</p>
-      </div>
-    </div>
 
-    <SectionCaption>
-      <CaptionTitle>Issue #6</CaptionTitle>
-      <CaptionDate>2020.07.31</CaptionDate>
-    </SectionCaption>
+    <SectionBread>
+      <BreadHome>Issue #5</BreadHome>
+      <BreadCurrent>2020-06-20</BreadCurrent>
+    </SectionBread>
 
     <SectionCellGroup>
-    {issue6.cells.map((cell, index) => (
+    {issue5.cells.map(cell => (
       <Cell
         title={cell.title}
         image={cell.image}
@@ -112,31 +95,27 @@ const IndexPage = () => (
         text={cell.text}
         tag={cell.tag}
         link={cell.link}
-        key={index}
-      />
+         />
     ))}
     </SectionCellGroup>
 
-    <SectionArchive id="IdArchive">
-      {issues.issue.map((issue, index) =>(
+    <SectionArchive>
+      {issues.issue.map(issue =>(
         <Archive
           title={issue.title}
           date={issue.date}
           link={issue.link}
           total={issue.total}
-          key={index}
         />
       ))}
     </SectionArchive>
 
-    <SectionAbout id="IdAbout">
-      <About></About>
-    </SectionAbout>
-  {/* <ComponentA></ComponentA> */}
-    <SectionSubscribe id="IdSubscribe">
+
+    <SectionSubscribe>
       <Subscribe></Subscribe>
     </SectionSubscribe>
+
   </Layout>
 )
 
-export default IndexPage
+export default SubscribePage
