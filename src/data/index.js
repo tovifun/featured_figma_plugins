2712e7e522889395c7c681a2a2d506860cb8a636
tovi <twowei@live.com>
2021-02-13 09:58:19 +0800

    issue 11

--- src/data/index.js
+++ src/data/index.js
@@ -3,7 +3,7 @@ const store = {
     tags: {},
     date: {}
 }
-for(let i=1; i<=10; i++){
+for(let i=1; i<=11; i++){
     const issue = require(`./issue${i}.json`)
     for(let cell of issue.cells){
         if(!cell) continue
